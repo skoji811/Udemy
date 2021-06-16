49ba300653b5ca7c51df1d03544b76a699cc19f2
skoji811 <s.koji811@gmail.com>
2021-06-16 18:43:37 +0900

    演習課題

--- MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -1,5 +1,45 @@
 package com.example.demo.app.survey;
 
-public class SurveyForm {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+public class SurveyForm {
+	
+	@Size( min = 1, max = 150, message = "正しい数値を入力して下さい")
+	private int age;
+	@Size( min = 1, max = 5, message = "1~5までの入力です")
+	private int satisfaction;
+	@Size( min = 1, max = 200, message = "200文字以内です")
+	@NotNull
+	private String comment;
+	
+	public SurveyForm() {
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getSatisfaction() {
+		return satisfaction;
+	}
+
+	public void setSatisfaction(int satisfaction) {
+		this.satisfaction = satisfaction;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 }
