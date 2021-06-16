3a674b086e99fb0d9da72136d2b8c17f2da1cfc9
skoji811 <s.koji811@gmail.com>
2021-06-16 18:50:23 +0900

    演習課題

--- MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,18 @@
 package com.example.demo.app.survey;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/survey")
 public class SurveyController {
+	
+	@GetMapping
+	public String form(Model model) {
+		model.addAttribute("title","調査フォーム");
+		return "survey/form";
+	}
 
 }
