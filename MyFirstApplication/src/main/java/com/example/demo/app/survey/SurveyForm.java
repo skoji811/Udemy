89f88e2789d575bfccabfc8a8bb5aff228ce8af9
skoji811 <s.koji811@gmail.com>
2021-06-17 22:54:56 +0900

    演習課題

--- MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -3,11 +3,13 @@ package com.example.demo.app.survey;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Range;
+
 public class SurveyForm {
 	
-	@Size( min = 1, max = 150, message = "正しい数値を入力して下さい")
+	@Range( min = 1, max = 150, message = "正しい数値を入力して下さい")
 	private int age;
-	@Size( min = 1, max = 5, message = "1~5までの入力です")
+	@Range	( min = 1, max = 5, message = "1~5までの入力です")
 	private int satisfaction;
 	@Size( min = 1, max = 200, message = "200文字以内です")
 	@NotNull
