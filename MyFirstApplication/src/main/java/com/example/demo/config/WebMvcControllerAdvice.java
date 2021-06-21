fbac8c101af15c05672b6d55df137470ab81332d
skoji811 <s.koji811@gmail.com>
2021-06-21 21:56:10 +0900

    first commit

--- MyFirstApplication/src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ MyFirstApplication/src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.example.demo.config;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 
 
@@ -19,6 +22,11 @@ public class WebMvcControllerAdvice {
     @InitBinder
     public void initBinder(WebDataBinder dataBinder) {
         dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public String handleException(EmptyResultDataAccessException e,Model model) {
+    	model.addAttribute("message",e);
+    	return "error/CustomPage";
     }
    
 }
